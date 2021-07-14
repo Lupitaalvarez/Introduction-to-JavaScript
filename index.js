64b0fe0ae7f3e4133d3195701eba64ce9ede8651
Lupita Alvarez <lupitaalvarez@Lupitas-MacBook-Air.local>
2021-07-13 17:04:17 -0700

    passed all

--- index.js
+++ index.js
@@ -32,8 +32,12 @@ Do the following:
 
    HINT: no function required
 */
-
-
+let tree = 3;
+let bush = 4;
+if(bush === 4){
+  tree = 5;
+}
+console.log(tree);
 
 
 
@@ -77,9 +81,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
+  
 }
+console.log('task2', dogYears(28));
 
 
 
@@ -110,9 +116,25 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1 && weight <= 5){
+    return weight * 0.05;
+  }else if(age >= 1 && weight >= 6 && weight <= 10){
+    return weight * 0.04;
+  }else if(age >= 1 && weight >= 11 && weight <= 15){
+    return weight * 0.03;
+  }else if(age >= 1 && weight > 15){
+    return weight * 0.02;
+  }else if(age < 1 && age >= 0.583){
+    return weight * 0.04;
+  }else if(age < 0.583 && age >= 0.333){
+    return weight * 0.05;
+  }else if(age < 0.333){
+    return weight * 0.1;
+  }
 }
+  /*add your code here*/
+
 
 
 
@@ -171,13 +193,14 @@ console.log('task 4', game('rock', computer));
 /*
 Using the miles function below do the following:
 1. Receive a number of kilometers
-2. Convert the number of kiolmeters received to miles
+2. Convert the number of kilometers received to miles
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
+console.log(miles(1.6));
 
 
 
@@ -189,9 +212,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
+console.log(feet(1));
 
 
 
@@ -229,7 +253,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
+function grade(score){
+  if(score >= 90){
+    return 'you got an A';
+  }else if(score < 90 && score >= 80){
+    return 'you got a B';
+  }else if(score < 80 && score >= 70){
+    return 'you got a C';
+  }else if(score < 70 && score >= 60){
+    return 'you got a D';
+  }else {
+    return 'you got an F';
+  }
 /*Your Code here */
 }
 
